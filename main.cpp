634de82c35e00824176f853a7e55637360fb9b07
夕木 夜間 <finn.yaki.0rz@gmail.com>
2026-04-22 16:42:54 +0900

    fix: bug fix

--- main.cpp
+++ main.cpp
@@ -8,19 +8,17 @@ T Min(T a, T b) {
 }
 
 template <>
-const char* Min<const char*>(const char* a, const char* b) {
-	return "数字以外は代入できません";
+char Min(char a, char b) {
+	std::cout << "数字以外は代入できません" << std::endl;
+	return '\0';
 }
-
-
-
 
 int main() {
 
 	std::cout << "整数型：" << Min(6, 9) << std::endl;
 	std::cout << "浮動小数点型：" << Min(3.14f, 8.71f) << std::endl;
 	std::cout << "倍精度浮動小数点型：" << Min(3.14, 1.71) << std::endl;
-	std::cout << "文字型：" << Min("A", "B") << std::endl;
+	std::cout << "文字型：" << Min('A', 'B') << std::endl;
 
     return 0;
 }
