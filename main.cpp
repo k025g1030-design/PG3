cfa0248460658f00b657ee0faa233653a3e09e7a
夕木 夜間 <finn.yaki.0rz@gmail.com>
2026-06-10 15:52:55 +0900

    update: 02_02_クラステンプレート

--- main.cpp
+++ main.cpp
@@ -1,71 +1,29 @@
 #include <iostream>
-#include <ctime>
-#include <thread>
 
-class Enemy {
+template<typename T>
+class Math {
 private:
-    using StateHandler = void (Enemy::*)();
+    T number1_;
+    T number2_;
 
-
-    StateHandler state_;
-
-    // 1. 接近状態
-    void Approach() {
-        std::cout << "[状態: 接近]" << std::endl;
-        // 次の状態（射撃）へ遷移
-        state_ = &Enemy::Shoot;
-        // 接近のための処理
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-    }
-
-    // 2. 射撃状態
-    void Shoot() {
-        std::cout << "[状態: 射撃]" << std::endl;
-        // 次の状態（離脱）へ遷移
-        state_ = &Enemy::Leave;
-        // 射撃のための処理
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
-
-    // 3. 離脱状態
-    void Leave() {
-        std::cout << "[状態: 離脱]" << std::endl;
-        // ループを止めるために nullptr にするか
-        state_ = nullptr;
-        // 離脱のための処理
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
+    
 
 public:
-    Enemy() : state_(&Enemy::Approach) {}
-
-    void Update() {
-        if (state_ != nullptr) {
-            // メンバ関数ポインタの呼び
-            (this->*state_)();
-        } else {
-            std::cout << "敵はすでに離脱しています。" << std::endl;
-        }
-    }
-
-    // すべての状態遷移が終わったかチェック
-    bool Finalize() const {
-        return state_ == nullptr;
+    Math(T n1, T n2) : number1_(n1), number2_(n2) {}
+    T Min() {
+        return (number1_ < number2_) ? number1_ : number2_;
     }
 };
 
 int main() {
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    std::cout << "--- Math判断開始 ---" << std::endl;
 
-    Enemy enemy;
-
-    std::cout << "--- 敵の行動開始 ---" << std::endl;
-
-    while (!enemy.Finalize()) {
-        enemy.Update();
-    }
-
-    std::cout << "--- 敵の行動終了 ---" << std::endl;
+    Math<int> mathInt(5, 10);
+    std::cout << "[int]最小値: " << mathInt.Min() << std::endl;
+    Math<double> mathDouble(3.14, 2.71);
+    std::cout << "[double]最小値: " << mathDouble.Min() << std::endl;
+    Math<float> mathFloat(0.14f, 2.71f);
+    std::cout << "[float]最小値: " << mathFloat.Min() << std::endl;
 
     return 0;
 }
