afd127555b212b5650238734ea82d200322cfce9
夕木 夜間 <finn.yaki.0rz@gmail.com>
2026-06-17 14:19:22 +0900

    update: 02_03_仮想関数とポリモーフィズム

--- main.cpp
+++ main.cpp
@@ -1,27 +1,55 @@
 #include <iostream>
+#include <ctime>
+#include <thread>
+#include <vector>
 
-template<typename T1, typename T2>
-class Math {
-private:
-    T1 number1_;
-    T2 number2_;
-
+class Cast {
 public:
-    Math(T1 n1, T2 n2) : number1_(n1), number2_(n2) {}
-    auto Min() -> decltype(number1_) {
-        return (number1_ < number2_) ? number1_ : number2_;
+    virtual void Spell() = 0;
+};
+
+class FireClazz : public Cast {
+public:
+    void Spell() override {
+        std::cout << "灰燼の園に灯をともせ。天を焦がす赤き翼、我が拳に宿りて業火と化せ。焼き尽くせ、地上のすべてを。" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "――『プロミネンス・フレイム』" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 };
 
-int main() {
-    std::cout << "--- Math判断開始 ---" << std::endl;
+class WaterClazz : public Cast {
+public:
+    void Spell() override {
+        std::cout << "静寂なる水の調べ、万物を浸す慈愛と深淵よ。巡り来る奔流となりて、理不尽を飲み込み、氷の棺へと変貌せよ。" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "――『アビス・ゼロ』" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+};
 
-    Math<int, int> mathInt(5, 10);
-    std::cout << "[int]最小値: " << mathInt.Min() << std::endl;
-    Math<double, double> mathDouble(3.14, 2.71);
-    std::cout << "[double]最小値: " << mathDouble.Min() << std::endl;
-    Math<float, float> mathFloat(0.14f, 2.71f);
-    std::cout << "[float]最小値: " << mathFloat.Min() << std::endl;
+class WindClazz : public Cast {
+public:
+    void Spell() override {
+        std::cout << "見えざる刃、大気そのものよ。切り裂け、虚空の彼方まで。我が意志に纏（まと）い、世界を切り刻む旋風となれ。" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "――『ゲイル・スラッシュ』" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+};
+
+
+
+int main() {
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+    std::cout << "--- 呪文を唱える ---" << std::endl;
+    Cast* cast = nullptr;
+    std::vector<Cast*> casts = { new FireClazz(), new WaterClazz(), new WindClazz() };
+
+    for (const auto& c : casts) { 
+        c->Spell();
+    }
 
     return 0;
 }
