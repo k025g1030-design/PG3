1d49fe772706ba54cad1d98c50e07ee93b8e2fe8
夕木 夜間 <finn.yaki.0rz@gmail.com>
2026-06-17 13:58:29 +0900

    update: fix 複数のテンプレートの定義

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 
-template<typename T>
+template<typename T1, typename T2>
 class Math {
 private:
-    T number1_;
-    T number2_;
-
-    
+    T1 number1_;
+    T2 number2_;
 
 public:
-    Math(T n1, T n2) : number1_(n1), number2_(n2) {}
-    T Min() {
+    Math(T1 n1, T2 n2) : number1_(n1), number2_(n2) {}
+    auto Min() -> decltype(number1_) {
         return (number1_ < number2_) ? number1_ : number2_;
     }
 };
@@ -18,11 +16,11 @@ public:
 int main() {
     std::cout << "--- Math判断開始 ---" << std::endl;
 
-    Math<int> mathInt(5, 10);
+    Math<int, int> mathInt(5, 10);
     std::cout << "[int]最小値: " << mathInt.Min() << std::endl;
-    Math<double> mathDouble(3.14, 2.71);
+    Math<double, double> mathDouble(3.14, 2.71);
     std::cout << "[double]最小値: " << mathDouble.Min() << std::endl;
-    Math<float> mathFloat(0.14f, 2.71f);
+    Math<float, float> mathFloat(0.14f, 2.71f);
     std::cout << "[float]最小値: " << mathFloat.Min() << std::endl;
 
     return 0;
