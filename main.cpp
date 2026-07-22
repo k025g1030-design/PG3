f5f39635d910e2323318c14dc5a1d8f39642c9e4
夕木 夜間 <finn.yaki.0rz@gmail.com>
2026-07-22 14:51:15 +0900

    update: 07_01_右辺値参照

--- main.cpp
+++ main.cpp
@@ -1,36 +1,40 @@
-#include <condition_variable>
+#include <chrono>
 #include <iostream>
-#include <mutex>
-#include <thread>
-
-std::mutex gMutex;
-std::condition_variable gCondition;
-int gCurrentThread = 1;
-
-void PrintThread(int threadNumber) {
-    // lock the mutex to ensure exclusive access to shared resources
-    std::unique_lock<std::mutex> lock(gMutex);
-
-    gCondition.wait(lock, [threadNumber]() {
-        return gCurrentThread == threadNumber;
-        });
-
-    std::cout << "thread " << threadNumber << std::endl;
-    ++gCurrentThread;
-
-    // unlock the mutex and notify other threads that they can proceed
-    lock.unlock();
-    gCondition.notify_all();
-}
+#include <string>
+#include <utility>
 
 int main() {
-    std::thread thread1(PrintThread, 1);
-    std::thread thread2(PrintThread, 2);
-    std::thread thread3(PrintThread, 3);
 
-    thread1.join();
-    thread2.join();
-    thread3.join();
+    std::string a(1000000, 'a');
+
+
+    const auto copyStart = std::chrono::steady_clock::now();
+    std::string copiedString(a);
+    const auto copyEnd = std::chrono::steady_clock::now();
+
+    const auto moveStart = std::chrono::steady_clock::now();
+    std::string movedString(std::move(a));
+    const auto moveEnd = std::chrono::steady_clock::now();
+
+    const auto copyTime =
+        std::chrono::duration_cast<std::chrono::microseconds>(
+            copyEnd - copyStart
+        );
+
+    const auto moveTime =
+        std::chrono::duration_cast<std::chrono::microseconds>(
+            moveEnd - moveStart
+        );
+
+    std::cout << "copy time : "
+        << copyTime.count() << " μs\n";
+
+    std::cout << "move time : "
+        << moveTime.count() << " μs\n";
+
+    volatile std::size_t totalSize =
+        copiedString.size() + movedString.size();
+    (void)totalSize;
 
     return 0;
 }
